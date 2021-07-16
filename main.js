0acdeb4f3fa64b23272b7dee94dcef4dae499b91
Humberto Navarro <humberto.navarro.dev@gmail.com>
2021-07-16 13:15:07 -0700

    wow very cool

--- main.js
+++ main.js
@@ -1,24 +1,32 @@
 var $addEntry = document.querySelector(".add-entry-btn");
-var $modal = document.querySelector(".modal")
-var $modalSubmit = document.querySelector(".modal-form-submit")
+var $modal = document.querySelector(".modal");
+var $modalSubmit = document.querySelector(".modal-form-submit");
+var $days = document.querySelectorAll('.dayofweek');
+var $week = document.querySelector('.week');
+var $scheduledEvents = document.querySelector('.scheduled-events');
 
-
-
-
-function openModal(event){
+function openModal(event) {
   $modal.className = "modal";
 }
-function closeModal(event){
+function closeModal(event) {
   $modal.className = "modal hidden"
 }
 
+function switchDays(day) {
+  var label = "Scheduled Events For ";
+  $scheduledEvents.textContent = label + day;
+}
 
 
 
 
 
 
-
-
-
+$week.addEventListener('click', function (event) {
+  if (!event.target.matches('.dayofweek')) {
+    return;
+  }
+  switchDays(event.target.textContent);
+});
+$modalSubmit.addEventListener("click", closeModal);
 $addEntry.addEventListener("click", openModal);
