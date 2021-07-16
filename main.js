b3f4a869768dd296cfe9055e1b5735d3c6027a2c
Paul Park <paulpark330@hotmail.com>
2021-07-16 14:33:40 -0700

    created table

--- main.js
+++ main.js
@@ -1,26 +1,21 @@
-var $addEntry = document.querySelector(".add-entry-btn");
-var $modal = document.querySelector(".modal");
-var $modalSubmit = document.querySelector(".modal-form-submit");
+var $addEntry = document.querySelector('.add-entry-btn');
+var $modal = document.querySelector('.modal');
+var $modalSubmit = document.querySelector('.modal-form-submit');
 var $days = document.querySelectorAll('.dayofweek');
 var $week = document.querySelector('.week');
 var $scheduledEvents = document.querySelector('.scheduled-events');
 
 function openModal(event) {
-  $modal.className = "modal";
+  $modal.className = 'modal';
 }
 function closeModal(event) {
-  $modal.className = "modal hidden"
+  $modal.className = 'modal hidden';
 }
 
 function switchDays(day) {
-  var label = "Scheduled Events For ";
+  var label = 'Scheduled Events For ';
   $scheduledEvents.textContent = label + day;
 }
-
-
-
-
-
 
 $week.addEventListener('click', function (event) {
   if (!event.target.matches('.dayofweek')) {
@@ -28,5 +23,5 @@ $week.addEventListener('click', function (event) {
   }
   switchDays(event.target.textContent);
 });
-$modalSubmit.addEventListener("click", closeModal);
-$addEntry.addEventListener("click", openModal);
+$modalSubmit.addEventListener('click', closeModal);
+$addEntry.addEventListener('click', openModal);
